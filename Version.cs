2241ba4d15b6d1e5d2345c2b7181e6dd4501249e
Paul Trampert <paul.trampert@gmail.com>
2016-05-11 00:41:02 -0400

    Fix prerelease detection.

--- Version.cs
+++ Version.cs
@@ -10,6 +10,6 @@
 
 [assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
 [assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0-build8")]
 
 
